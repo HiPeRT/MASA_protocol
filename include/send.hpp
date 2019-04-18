54d623023622b01255fb0579fdd0af7bfc0a25b4
Roberto Cavicchioli <roberto.cavicchioli@unimore.it>
2019-04-18 17:37:51 +0200

    First Commit

---
+++ include/send.hpp
@@ -0,0 +1,173 @@
+#ifndef SEND_H
+#define SEND_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h> //socket
+#include <arpa/inet.h>  //inet_addr
+#include <unistd.h>     //write
+
+#include "serialize.hpp"
+
+class Communicator{
+
+    public: 
+    static const int message_size = 50000;
+    int sock;
+    int port; 
+    int socket_opened = 0;
+    std::string ip;
+    char client_message[message_size];
+
+    Communicator(){}
+
+    int get_socket(){
+        return sock;
+    }
+
+    void serialize_coords(Message *m, std::stringbuf* buf)
+    {
+        std::ostream os(buf);
+        cereal::PortableBinaryOutputArchive archive(os);
+        archive(*m);
+    }
+
+    void deserialize_coords(char *buffer, Message *m)
+    {
+        std::stringbuf buf(buffer);
+        std::istream is(&buf);
+        cereal::PortableBinaryInputArchive retrieve(is);
+        retrieve(*m);
+    }
+
+    void deserialize_coords(std::string s, Message *m)
+    {
+        std::istringstream is(s);
+        cereal::PortableBinaryInputArchive retrieve(is);
+        retrieve(*m);
+    }
+    
+
+    int open_client_socket(char *ip, int port)
+    {
+        struct sockaddr_in server;
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock == -1)
+        {
+            printf("Could not create socket");
+        }
+        puts("Socket created");
+
+        this->ip = std::string(ip);
+        this->port = port;
+        server.sin_addr.s_addr = inet_addr(ip);
+        server.sin_family = AF_INET;
+        server.sin_port = htons(port);
+
+        /*connect to remote server*/
+        if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+        {
+            perror("connect failed. Error");
+            socket_opened = 0;
+            return 0;
+        }
+        puts("Connected\n");
+        socket_opened = 1;
+        return 1;
+    }
+
+    int open_server_socket(int port)
+    {
+        struct sockaddr_in server;
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock == -1)
+        {
+            printf("Could not create socket");
+        }
+        puts("Socket created");
+
+        this->port = port;
+        //Prepare the sockaddr_in structure
+        server.sin_family = AF_INET;
+        server.sin_addr.s_addr = INADDR_ANY;
+        server.sin_port = htons(port);
+        //Bind
+        if (bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+        {
+            //print the error message
+            perror("bind failed. Error");
+            return 1;
+        }
+        puts("bind done");
+
+        //Listen
+        listen(sock, 3);
+        return 1;
+    }
+
+    int send_message(Message *m)
+    {
+        /*serialize coords*/
+        std::stringbuf *message = new std::stringbuf();
+        serialize_coords(m, message);
+
+        /*open socket if not already opened*/
+        if (socket_opened == 0)
+        {
+            std::vector<char> cstr(ip.c_str(), ip.c_str() + ip.size() + 1);
+            int res = open_client_socket(cstr.data(), port);
+            if(res)
+                printf("Socket opened!\n");
+            else
+            {
+                printf("Problem: socket NOT opened!\n");
+                return 0;
+            }
+        }
+
+
+        /*send message to server*/
+        if (send(sock, message->str().data(), message->str().length(), 0) < 0)
+        {
+            puts("Send failed");
+            socket_opened = 0;
+        }
+
+        delete message;
+        return 1;
+    }
+
+    int receive_message(int socket_desc, Message *m)
+    {
+        int read_size;
+        memset(client_message, 0, message_size);
+
+        while ((read_size = recv(socket_desc, client_message, message_size, 0)) > 0)
+        {
+            std::string s((char *)client_message, message_size);
+            deserialize_coords(s, m);
+            return 0;
+        }
+
+        if (read_size == 0)
+        {
+            puts("Client disconnected");
+            fflush(stdout);
+            return 1;
+        }
+        else if (read_size == -1)
+        {
+            perror("recv failed");
+            return 1;
+        }
+        return 1;
+    }
+
+};
+
+
+
+#endif /*SEND_H*/
