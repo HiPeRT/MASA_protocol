c9e145820a3baf09237742c5d6bcedcc7cf83306
Cristian Gabbi <224915@studenti.unimore.it>
2019-10-25 15:30:10 +0200

    serialize.hpp fixed to contain all different vehicle structure

--- include/serialize.hpp
+++ include/serialize.hpp
@@ -56,33 +56,15 @@ struct RoadUser{
 
 
 //Hypothetic structure for vehicle
-struct RoadVehicle {
+struct Vehicle {
     float latitude;
     float longitude;
     uint8_t speed;
     uint8_t orientation;
-    //Categories category;
     uint8_t acc_x;
     uint8_t acc_y;
     uint8_t rpm;
     uint8_t gear;
-    bool is_distracted;
-    //bool we_steer;
-    float  km_to_automation;
-    int8_t steer_value;
-    //autopilot fields
-    //bool    autopilot;
-    float   drive_length;
-    float   start_auto;
-    float   end_auto;
-    float   fallback_pos;
-    uint8_t car_pos;
-
-    //uint16_t yaw;
-    //uint8_t steer_ang;
-    //uint8_t gyr_x;
-    //uint8_t gyr_y;
-
 
     // More fields to be added here
 
@@ -94,25 +76,72 @@ struct RoadVehicle {
                 longitude,
                 speed,
                 orientation,
-                //category,
+                acc_x,
+                acc_y,
+                rpm,
+                gear
+              );
+    }
+};
+
+
+
+//Generic struct to contain status trigger
+struct StatusDriver: Vehicle {
+    bool is_distracted;
+
+    // More fields to be added here
+
+    template<class Archive>
+    void serialize(Archive &archive)
+    {
+        archive(
+                latitude,
+                longitude,
+                speed,
+                orientation,
+                acc_x,
+                acc_y,
+                rpm,
+                gear,
+		is_distracted
+              );
+    }
+};
+
+
+
+//Vehicle struct for Christine HMI use-case scenario
+struct ChristineVehicle : Vehicle {
+    bool is_distracted;
+    float  km_to_automation;
+    int8_t steer_value;
+    float   drive_length;
+    float   start_auto;
+    float   end_auto;
+    float   fallback_pos;
+    uint8_t car_pos;    
+
+    template<class Archive>
+    void serialize(Archive &archive)
+    {
+        archive(
+                latitude,
+                longitude,
+                speed,
+                orientation,
                 acc_x,
                 acc_y,
                 rpm,
                 gear,
                 is_distracted,
-		            //we_steer,
                 km_to_automation,
                 steer_value,
-		            //autopilot,
                 drive_length,
                 start_auto,
                 end_auto,
                 fallback_pos,
                 car_pos
-                //yaw,
-                //steer_ang,
-                //gyr_x,
-                //gyr_y
               );
     }
 };
@@ -126,12 +155,24 @@ struct Message{
     uint16_t num_objects;
     std::vector<RoadUser> objects;
     std::vector<TrafficLight> lights;
-    RoadVehicle vehicle;
 
     template<class Archive>
     void serialize(Archive & archive)
     {
-        archive( cam_idx, t_stamp_ms, num_objects, objects, lights, vehicle);
+        archive( cam_idx, t_stamp_ms, num_objects, objects, lights);
+    }
+};
+
+
+
+//Message struct for Christine HMI use-case scenario
+struct ChristineMessage : Message {
+    ChristineVehicle cv;
+
+    template<class Archive>
+    void serialize(Archive & archive)
+    {
+        archive( cam_idx, t_stamp_ms, num_objects, objects, lights, cv);
     }
 };
 
