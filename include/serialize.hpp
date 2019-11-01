33d1c2221572595c7a8025236ffdc47850a5fd35
Cristian Gabbi <224915@studenti.unimore.it>
2019-11-01 19:14:49 +0100

    Data structures updated in serialize.hpp

--- include/serialize.hpp
+++ include/serialize.hpp
@@ -147,7 +147,7 @@ struct ChristineMessage: Message{
     template<class Archive>
     void serialize(Archive & archive)
     {
-        archive(cam_idx, t_stamp_ms, cv);
+        archive(cam_idx, t_stamp_ms, num_objects, objects, lights, cv);
     }
 };
 
