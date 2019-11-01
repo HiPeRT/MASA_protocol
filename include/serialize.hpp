77e3f035d19619f1f3b8e8af4158b9476bd0dea4
Cristian Gabbi <224915@studenti.unimore.it>
2019-11-01 19:35:12 +0100

    Data structures updated in serialize.hpp

--- include/serialize.hpp
+++ include/serialize.hpp
@@ -130,12 +130,12 @@ struct Message{
     uint16_t num_objects;
     std::vector<RoadUser> objects;
     std::vector<TrafficLight> lights;
-    ChristineVehicle cv;
+    //ChristineVehicle cv;
 
     template<class Archive>
     void serialize(Archive & archive)
     {
-        archive( cam_idx, t_stamp_ms, num_objects, objects, lights, cv);
+        archive( cam_idx, t_stamp_ms, num_objects, objects, lights);
     }
 };
 
